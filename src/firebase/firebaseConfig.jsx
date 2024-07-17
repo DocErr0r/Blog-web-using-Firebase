8f15e1aa11f60701630d99bcf383bcf3d3e68aea
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2024-07-17 15:54:51 +0530

    intialize the firebase configureation

---
+++ src/firebase/firebaseConfig.jsx
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBix9aQrBRmxuFmv-D3vQE4BzkF-1XcZqA',
+    authDomain: 'blog-nits.firebaseapp.com',
+    projectId: 'blog-nits',
+    storageBucket: 'blog-nits.appspot.com',
+    messagingSenderId: '85606260535',
+    appId: '1:85606260535:web:f2e448344c9c1c8bd8c4b2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
